2ac015d5c8e0ec25bb9585776a020fd60b811ed4
Marudhupandian Maruthan <mmp1985@gmail.com>
2021-02-14 20:47:10 +0530

    Initial Commit

---
+++ src/main/java/edu/authentcate/api/AuthenticateApiSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package edu.authentcate.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticateApiSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticateApiSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/authentcate/api/AuthenticateApiSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.authentcate.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticateApiSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
