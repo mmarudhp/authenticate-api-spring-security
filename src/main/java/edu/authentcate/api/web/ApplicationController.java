ef2a0828c2cc14743a48222ba6c185f5acaf2a79
Marudhupandian Maruthan <mmp1985@gmail.com>
2021-02-14 23:25:50 +0530

    Add comments in controller end points and server port as 8085

--- src/main/java/edu/authentcate/api/web/ApplicationController.java
+++ src/main/java/edu/authentcate/api/web/ApplicationController.java
@@ -3,6 +3,7 @@ package edu.authentcate.api.web;
 import edu.authentcate.api.model.TermsAndConditions;
 import edu.authentcate.api.model.User;
 import edu.authentcate.api.service.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +14,20 @@ import java.util.List;
 @RequestMapping("/api")
 public class ApplicationController {
 
+    @Autowired
     private ApplicationService service;
 
+    /**
+     * This is post login API which does require an authentication.
+     **/
     @GetMapping("/users")
     public List<User> getAllUsers() {
         return service.getAllUsers();
     }
 
+    /**
+     * This is pre login API which does not require an authentication.
+     **/
     @GetMapping("/terms")
     public TermsAndConditions getTermsAndConditions() {
         return service.getTermsAndConditions();
